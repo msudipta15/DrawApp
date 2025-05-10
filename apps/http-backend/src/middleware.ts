b5cddc81f6957a9d2ce5c6569592417610d3e457
Sudipta Mondal <techtv70@gmail.com>
2025-05-10 19:32:13 +0530

    .

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/token";
+
+export function userAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.token;
+  if (!token) {
+    res.status(406).json({ msg: "No token found" });
+    return;
+  }
+  const decoded = jwt.verify(token?.toString(), JWT_SECRET) as {
+    user_id: String;
+  };
+
+  if (!decoded) {
+    res.status(406).json({ msg: "You are not signed in !" });
+    return;
+  }
+
+  try {
+    const userid = decoded.user_id;
+    req.id = userid;
+    next();
+  } catch (error) {
+    res.status(406).json({ msg: "something went wrong" });
+  }
+}
