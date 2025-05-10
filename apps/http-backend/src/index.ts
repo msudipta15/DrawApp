3baa57536fbd38e67a5028735181c2003ae8cdd5
Sudipta Mondal <techtv70@gmail.com>
2025-05-10 15:46:38 +0530

    .

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { Request, Response } from "express";
 import { prismaClient } from "@repo/db/client";
 import { SignupSchema, SigninSchema, RoomSchema } from "@repo/common/types";
+import { JWT_SECRET } from "@repo/backend-common/token";
+
 const app = express();
 
 app.use(express.json());
+
+console.log(JWT_SECRET);
 
 app.post("/signup", async function (req: Request, res: Response) {
   const safeparse = SignupSchema.safeParse(req.body);
