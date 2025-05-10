5bbe074584efcb3ee1a9dbf2ad8ad3e43a2986ca
Sudipta Mondal <techtv70@gmail.com>
2025-05-10 17:59:40 +0530

    created sign up end point  and connected with database

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -5,33 +5,76 @@ import express from "express";
 import { Request, Response } from "express";
 import { prismaClient } from "@repo/db/client";
 import { SignupSchema, SigninSchema, RoomSchema } from "@repo/common/types";
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/token";
 
 const app = express();
 
 app.use(express.json());
 
-console.log(JWT_SECRET);
-
 app.post("/signup", async function (req: Request, res: Response) {
   const safeparse = SignupSchema.safeParse(req.body);
 
   if (safeparse.error) {
-    res.json({ msg: "Invalid Input" });
-  } else {
-    try {
-      const user = await prismaClient.user.create({
-        data: {
-          email: safeparse.data?.email,
-          password: safeparse.data?.password,
-          name: safeparse.data?.name,
-        },
-      });
+    res.status(406).json({ msg: "Invalid Input" });
+    return;
+  }
 
-      res.json({ user_id: user.id });
-    } catch (error) {
-      console.log(error);
-    }
+  const finduser = await prismaClient.user.findFirst({
+    where: { email: safeparse.data.email },
+  });
+
+  if (finduser) {
+    res.status(411).json({ msg: "user already exists with this email" });
+    return;
+  }
+
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: safeparse.data?.email,
+        password: safeparse.data?.password,
+        name: safeparse.data?.name,
+      },
+    });
+
+    res.status(200).json({ user_id: user.id });
+  } catch (error) {
+    console.log(error);
+    res.status(411).json({ msg: "something went wrong" });
+  }
+});
+
+app.post("/signin", async function (req: Request, res: Response) {
+  const safeparse = SigninSchema.safeParse(req.body);
+
+  if (safeparse.error) {
+    res.status(406).json({ msg: "Invalid Input" });
+    return;
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: safeparse.data.email,
+    },
+  });
+
+  if (!user) {
+    res.status(406).json({ msg: "No user found" });
+    return;
+  }
+
+  if (user.password !== safeparse.data.password) {
+    res.status(406).json({ msg: "Incorrect Password" });
+    return;
+  }
+
+  try {
+    const token = jwt.sign({ user_id: user.id }, JWT_SECRET);
+    res.status(200).json({ token: token });
+  } catch (error) {
+    console.log(error);
+    res.status(402).json({ msg: "something went wrong" });
   }
 });
 
--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,1 +1,5 @@
+if (!process.env.JWT_SECRET) {
+  throw new Error("jwt_key is not defined");
+}
+
 export const JWT_SECRET = process.env.JWT_SECRET;
